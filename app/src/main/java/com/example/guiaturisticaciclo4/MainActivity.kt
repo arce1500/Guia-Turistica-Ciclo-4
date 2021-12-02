2e2c9e8c2ebb800c652e222b3ce1d84a00087afc
Milton Vargas <hadez2505@gmail.com>
2021-12-02 15:21:01 -0500

    Cambios

--- app/src/main/java/com/example/guiaturisticaciclo4/MainActivity.kt
+++ app/src/main/java/com/example/guiaturisticaciclo4/MainActivity.kt
@@ -8,7 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cardview)
-        val getList = Utils(this).getList()
+        val getList = Utils(this).getJsonDataFromAsset(applicationContext, "lugares.json")
         println(getList)
+
     }
 }
--- app/src/main/java/com/example/guiaturisticaciclo4/utils/Utils.kt
+++ app/src/main/java/com/example/guiaturisticaciclo4/utils/Utils.kt
@@ -2,9 +2,22 @@ package com.example.guiaturisticaciclo4.utils
 
 import android.content.Context
 import com.example.guiaturisticaciclo4.R
+import java.io.IOException
 
 class Utils (val context: Context){
     fun getList (): Array<String>{
         return context.resources.getStringArray(R.array.lista_uno)
     }
+
+    fun getJsonDataFromAsset(context: Context, fileName: String): String? {
+        val jsonString: String
+        try {
+            jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+            return null
+        }
+        return jsonString
+    }
+
 }
