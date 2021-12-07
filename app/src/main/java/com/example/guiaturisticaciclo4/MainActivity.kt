196a4a0696984a29ef6da7eabfa747823ecbad28
Milton Vargas <hadez2505@gmail.com>
2021-12-07 15:28:10 -0500

    Cambios

--- app/src/main/java/com/example/guiaturisticaciclo4/MainActivity.kt
+++ app/src/main/java/com/example/guiaturisticaciclo4/MainActivity.kt
@@ -3,14 +3,22 @@ package com.example.guiaturisticaciclo4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.guiaturisticaciclo4.utils.Utils
-import com.google.gson.JsonElement
+import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cardview)
-        val lugaresList:Array<JsonElement> = Utils(this).getJsonDataFromAsset(applicationContext, "lugares.json")
-        println(lugaresList?.get(0).name)
+        val lugaresList = Utils(this).getJsonDataFromAsset(applicationContext, "lugares.json")
+        val array = JSONArray(lugaresList)
+        println(array[1])
+        val nombres: ArrayList<String> = ArrayList()
+        for (i in 0 until array.length()){
+            val cityInfo = array.getJSONObject(i)
+            nombres.add(cityInfo.getString("name"))
+        }
+        println(nombres)
+
 
     }
 }
--- app/src/main/java/com/example/guiaturisticaciclo4/utils/lugaresAdapter.kt
+++ app/src/main/java/com/example/guiaturisticaciclo4/utils/lugaresAdapter.kt
@@ -5,9 +5,14 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.JsonElement
 
-class lugaresAdapter(val lugares:Array<JsonElement>):RecyclerView.Adapter<lugaresAdapter.lugarHolder>(){
+class lugaresAdapter(
+    private var lugaresNames: ArrayList<String>,
+    private var lugaresdescription: ArrayList<String>,
+    private var lugareslocation: ArrayList<String>,
+    private var lugaresimageUrl: ArrayList<String>,
+    private var lugarespuntuacion: ArrayList<String>,
 
-
+    ):RecyclerView.Adapter<lugaresAdapter.lugarHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): lugarHolder {
         TODO("Not yet implemented")
@@ -17,7 +22,8 @@ class lugaresAdapter(val lugares:Array<JsonElement>):RecyclerView.Adapter<lugare
         TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int = lugares.size
+    override fun getItemCount(): Int = lugaresNames.size
+
 
     class lugarHolder(val view:View):RecyclerView.ViewHolder(view) {
 
