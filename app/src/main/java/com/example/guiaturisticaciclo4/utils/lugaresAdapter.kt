a6be85defdba4ebf04b331bdef9f9f5fab09243a
Milton Vargas <hadez2505@gmail.com>
2021-12-08 13:49:56 -0500

    Adapter

--- app/src/main/java/com/example/guiaturisticaciclo4/utils/lugaresAdapter.kt
+++ app/src/main/java/com/example/guiaturisticaciclo4/utils/lugaresAdapter.kt
@@ -1,28 +1,32 @@
 package com.example.guiaturisticaciclo4.utils
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.guiaturisticaciclo4.R
 import com.example.guiaturisticaciclo4.SitiosTuristicos
-import com.google.gson.JsonElement
 
-class lugaresAdapter(val lugares:ArrayList<SitiosTuristicos>):RecyclerView.Adapter<lugaresAdapter.lugarHolder>(){
+class lugaresAdapter(val lugares:ArrayList<SitiosTuristicos>):RecyclerView.Adapter<lugaresAdapter.LugarHolder>(){
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): lugarHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LugarHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.cardview, parent, false)
+        return  LugarHolder(v)
     }
 
-    override fun onBindViewHolder(holder: lugarHolder, position: Int) {
-        holder.render(lugares[position])
+    override fun onBindViewHolder(holder: LugarHolder, position: Int) {
+        holder.render(lugares [position])
     }
 
     override fun getItemCount(): Int = lugares.size
 
 
-    class lugarHolder(val view:View):RecyclerView.ViewHolder(view) {
-        fun render(lugares:SitiosTuristicos){
-
+    class LugarHolder(private val view:View):RecyclerView.ViewHolder(view) {
+        fun render(lugares:SitiosTuristicos) {
+                view.tv
         }
     }
 
 }
+
+
